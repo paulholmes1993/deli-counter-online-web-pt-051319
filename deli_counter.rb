ef099acbf83334dca388ba3328e2c6643b3b5d00
Paul Holmes <paulholmes1993@gmail.com>
2019-06-06 23:16:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    number >= 1
-    katz_deli.each do |num|
-      line_positions << :"#(number). (customer)"
+    number = 1
+    katz_deli.each do |customer|
+      line_positions << "#(number).(customer)"
       number += 1
     end
     puts "The line is currently: #{line_positions.join(" ")}"
