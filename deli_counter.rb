3968fdef92e64365903971d783c71507f36b388a
Paul Holmes <paulholmes1993@gmail.com>
2019-06-06 23:29:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
   else
     number = 1
     katz_deli.each do |customer|
-      line_positions << "#(number).(customer)"
+      line_positions << "#(number).#(customer)"
       number += 1
     end
     puts "The line is currently: #{line_positions.join(" ")}"
