b6e3db30ab49252f5ec673b7fe1ed1c1adca38aa
Paul Holmes <paulholmes1993@gmail.com>
2019-06-06 23:15:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 katz_deli = []
 
 def line(katz_deli)
-  line_positions = []
-  if katz_deli == 0
+  line_positions = [];
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
     number >= 1
