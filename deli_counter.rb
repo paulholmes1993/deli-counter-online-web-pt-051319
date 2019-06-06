9aa8c150666dd911ed59f4e045d769a76776150f
Paul Holmes <paulholmes1993@gmail.com>
2019-06-06 23:41:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,13 +6,14 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     number = 1
-    katz_deli.each do | customer |
-      line_positions << "#(number).#(customer)"
+    katz_deli.each do |customer|
+      line_positions << "#{number}. #{customer}"
       number += 1
     end
     puts "The line is currently: #{line_positions.join(" ")}"
   end
 end
+
 
 def take_a_number(katz_deli, name)
   katz_deli << name
